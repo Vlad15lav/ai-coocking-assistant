2d9a3b842db78d7fab72c13618444ae364affd79
Vlad15lav <vladislav.viryasov@gmail.com>
2024-10-08 19:11:44 +0300

    add format_docs func

--- src/tools/utils.py
+++ src/tools/utils.py
@@ -13,3 +13,15 @@ def clean_input(input_string: str) -> str:
     clean_string = re.sub(r'(^|[^\w])[\?!]+', r'\1', input_string)
 
     return clean_string
+
+
+def format_docs(docs) -> str:
+    """Форматирование документов в единную строку
+
+    Args:
+        docs: Документы от ретривера
+
+    Returns:
+        (str): Строка со всеми документами
+    """
+    return "\n\n".join([d.page_content for d in docs])
